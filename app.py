a9338a3b4b53c36e5479048827fbd610225e01e5
shivanshgupta93 <shivansh.gupta@mygeorgian.ca>
2019-10-29 03:32:39 -0400

    Updated cron job

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import json
 import requests
 from flask import Flask, render_template
 from routes import api
-####from apscheduler.schedulers.background import BackgroundScheduler
-#####from jobs.cron import cron_job
+from apscheduler.schedulers.background import BackgroundScheduler
+from jobs.cron import cron_job
 
 app = Flask(__name__, template_folder='static')
 
@@ -14,7 +14,7 @@ def run_cron_job():
     cron_job()
 
 sched = BackgroundScheduler(daemon=True)
-sched.add_job(run_cron_job, 'cron', day_of_week='tue', hour=3, minute=32)
+sched.add_job(run_cron_job, 'cron', day_of_week='tue', hour=3, minute=35)
 sched.start()
 
 
