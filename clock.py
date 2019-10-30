9894e4354caa15ecc27dc3ba18f8c342f45fb206
shivanshgupta93 <shivansh.gupta@mygeorgian.ca>
2019-10-30 19:53:24 -0400

    updated apscheduler

--- clock.py
+++ clock.py
@@ -3,5 +3,5 @@ from jobs.cron import cron_job
 
 ###sched = BlockingScheduler(daemon=True)
 sched = BlockingScheduler()
-sched.add_job(cron_job, 'cron', day_of_week='wed', hour=8, minute=0)
+sched.add_job(cron_job, 'cron', day_of_week='wed', hour=20, minute=0)
 sched.start()
