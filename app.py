d05d1f69164c91117e02c2c20fc78b06cf4f2af7
shivanshgupta93 <shivansh.gupta@mygeorgian.ca>
2019-10-30 21:00:21 -0400

    updated cron job

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 import os
-import json
-import requests
 from flask import Flask, render_template
 from routes import api
-from apscheduler.schedulers.blocking import BlockingScheduler
-from jobs.cron import cron_job
 
 app = Flask(__name__, template_folder='static')
 
 app.register_blueprint(api)
-
-cron_job()
-
 
 @app.route("/")
 
--- clock.py
+++ clock.py
@@ -1,16 +1,9 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from jobs.cron import cron_job
 
-''''
-###sched = BlockingScheduler(daemon=True)
-sched = BlockingScheduler()
-sched.add_job(cron_job, 'cron', day_of_week='thu', hour=0, minute=5)
-sched.start()
-'''
-
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='thu', hour=0, minute=15)
+@sched.scheduled_job('cron', day_of_week=2, hour=21, minute=15, timezone = 'America/New_York')
 def scheduled_job():
     cron_job()
 
