8211737454016de0f32f84a0257882c8cf3dc1f3
shivanshgupta93 <shivansh.gupta@mygeorgian.ca>
2019-10-30 21:20:03 -0400

    Updated clock.py

--- clock.py
+++ clock.py
@@ -3,8 +3,11 @@ from jobs.cron import cron_job
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron',day_of_week=2,hour=21,minute=15,timezone ='America/New_York')
+@sched.scheduled_job('cron',day_of_week=3,hour=1,minute=25)
 def scheduled_job():
+    print("Running clock file")
     cron_job()
 
 sched.start()
+
+###,timezone ='America/New_York
