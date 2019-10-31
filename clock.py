538f4b042423685f97dd774bb23fb212b4ca0c07
shivanshgupta93 <shivansh.gupta@mygeorgian.ca>
2019-10-30 21:05:03 -0400

    Updated cron job

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from jobs.cron import cron_job
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week=2, hour=21, minute=15, timezone = 'America/New_York')
+@sched.scheduled_job('cron',day_of_week=2,hour=21,minute=15,timezone ='America/New_York')
 def scheduled_job():
     cron_job()
 
