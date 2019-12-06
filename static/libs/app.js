a2dc45e551a59b704ed621baa0e091441be0fa7a
shivanshgupta93 <shivansh.gupta@mygeorgian.ca>
2019-12-06 02:19:00 -0500

    Updated Js

--- static/libs/app.js
+++ static/libs/app.js
@@ -28,13 +28,13 @@ document.getElementById("selectGenre").onchange = function(){
     };
 
 function artists(tag_value){
-    $.get("/api/artists/all", (artists, err) => {
+    $.get("/api/artists?tag="+tag_value, (artists, err) => {
         if (err !== "success") console.error(err);
         console.log(tag_value);
         if (artists && Array.isArray(artists.data)) {
             const tags = [...new Set(artists.data.map(artist => artist.tag))] // function(artist) {return artist.tag}
             const insert_dates = [...new Set(artists.data.map(artist => artist.inserted_date))]
-            const data = tags.map(tag => artists.data.filter(artist => artist.tag == tag_value).length)
+            const data = insert_dates.map(inserted_date => artists.data.filter(artist => artist.inserted_date == inserted_date).length)
             const ctx = document.getElementById('artists').getContext('2d');
             console.log(data);
             const artists_chart = new Chart(ctx, {
@@ -44,22 +44,22 @@ function artists(tag_value){
                     datasets: [{
                         label: 'Artists based on tags',
                         data: data,
-                        // backgroundColor: [
-                        //     'rgba(255, 99, 132, 0.2)',
-                        //     'rgba(54, 162, 235, 0.2)',
-                        //     'rgba(255, 206, 86, 0.2)',
-                        //     'rgba(75, 192, 192, 0.2)',
-                        //     'rgba(153, 102, 255, 0.2)',
-                        //     'rgba(255, 159, 64, 0.2)'
-                        // ],
-                        // borderColor: [
-                        //     'rgba(255, 99, 132, 1)',
-                        //     'rgba(54, 162, 235, 1)',
-                        //     'rgba(255, 206, 86, 1)',
-                        //     'rgba(75, 192, 192, 1)',
-                        //     'rgba(153, 102, 255, 1)',
-                        //     'rgba(255, 159, 64, 1)'
-                        // ],
+                         backgroundColor: [
+                             'rgba(255, 99, 132, 0.2)',
+                             'rgba(54, 162, 235, 0.2)',
+                             'rgba(255, 206, 86, 0.2)',
+                             'rgba(75, 192, 192, 0.2)',
+                             'rgba(153, 102, 255, 0.2)',
+                             'rgba(255, 159, 64, 0.2)'
+                         ],
+                         borderColor: [
+                             'rgba(255, 99, 132, 1)',
+                             'rgba(54, 162, 235, 1)',
+                             'rgba(255, 206, 86, 1)',
+                             'rgba(75, 192, 192, 1)',
+                             'rgba(153, 102, 255, 1)',
+                             'rgba(255, 159, 64, 1)'
+                         ],
                         borderWidth: 1
                     }]
                 },
