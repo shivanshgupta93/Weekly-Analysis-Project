81274d3c07543b96dd8a409e1cb9633b716bb521
shivanshgupta93 <shivansh.gupta@mygeorgian.ca>
2019-12-08 04:54:44 -0500

    Updated js

--- static/libs/app.js
+++ static/libs/app.js
@@ -1,6 +1,18 @@
 'use strict';
 
-
+var colors = ["#000000", "#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059",
+"#FFDBE5", "#7A4900", "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87",
+"#5A0007", "#809693",  "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80",
+"#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9", "#B903AA", "#D16100",
+"#DDEFFF", "#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F",
+"#372101", "#FFB500", "#C2FFED", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09",
+"#00489C", "#6F0062", "#0CBD66", "#EEC3FF", "#456D75", "#B77B68", "#7A87A1", "#788D66",
+"#885578", "#FAD09F", "#FF8A9A", "#D157A0", "#BEC459", "#456648", "#0086ED", "#886F4C",
+"#34362D", "#B4A8BD", "#00A6AA", "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81",
+"#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757", "#C8A1A1", "#1E6E00",
+"#7900D7", "#A77500", "#6367A9", "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700",
+"#549E79", "#FFF69F", "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329",
+"#5B4534", "#FDE8DC", "#404E55", "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C"];
 var first_tag;
 $(document).ready(() => {
         $.get("/api/tags", (tags, err) => {
@@ -37,7 +49,7 @@ document.getElementById("selectGenre").onchange = function(){
             if (top_artists && Array.isArray(top_artists.data)) {
                 var insert_dates = []
                 var artist_name = ""
-                const clockContainer=document.querySelector(".topartistdata")
+                const artist_container = document.querySelector(".topartistdata")
                 for(var i=0; i<top_artists.data.length; i++)
                     {
                         insert_dates.push(top_artists.data[i]["inserted_date"])
@@ -52,7 +64,14 @@ document.getElementById("selectGenre").onchange = function(){
                         }
                 }
 
-                clockContainer.innerText = artist_name;
+                if((tag_value == "Choose a Genre"))
+                {
+                    artist_container.innerText = "Select a Genre";
+                }
+                else
+                {
+                    artist_container.innerText = artist_name;
+                }
             }
         });
     }
@@ -64,7 +83,7 @@ document.getElementById("selectGenre").onchange = function(){
                 var insert_dates = []
                 var album_name = ""
                 var artist_name = ""
-                const clockContainer=document.querySelector(".topalbumdata")
+                const album_container = document.querySelector(".topalbumdata")
                 for(var i=0; i<top_albums.data.length; i++)
                     {
                         insert_dates.push(top_albums.data[i]["inserted_date"])
@@ -80,7 +99,14 @@ document.getElementById("selectGenre").onchange = function(){
                         }
                 }
 
-                clockContainer.innerText = album_name + " by " + artist_name;
+                if((tag_value == "Choose a Genre"))
+                {
+                    album_container.innerText = "Select a Genre";
+                }
+                else
+                {
+                    album_container.innerText = album_name + " by " + artist_name;
+                }
             }
         });
     }
@@ -92,7 +118,7 @@ document.getElementById("selectGenre").onchange = function(){
                 var insert_dates = []
                 var track_name = ""
                 var artist_name = ""
-                const clockContainer=document.querySelector(".toptrackdata")
+                const track_container = document.querySelector(".toptrackdata")
                 for(var i=0; i<top_tracks.data.length; i++)
                     {
                         insert_dates.push(top_tracks.data[i]["inserted_date"])
@@ -108,7 +134,14 @@ document.getElementById("selectGenre").onchange = function(){
                         }
                 }
 
-                clockContainer.innerText = track_name + " by " + artist_name;
+                if((tag_value == "Choose a Genre"))
+                {
+                    track_container.innerText = "Select a Genre";
+                }
+                else
+                {
+                    track_container.innerText = track_name + " by " + artist_name;
+                }
             }
         });
     }
@@ -117,61 +150,71 @@ document.getElementById("selectGenre").onchange = function(){
     function artists(tag_value){
     $.get("/api/artists?tag="+tag_value, (artists, err) => {
         if (err !== "success") console.error(err);
-        console.log(tag_value);
         if (artists && Array.isArray(artists.data)) {
-            const tags = [...new Set(artists.data.map(artist => artist.tag))] // function(artist) {return artist.tag}
             const insert_dates = [...new Set(artists.data.map(artist => artist.inserted_date))]
-            const data = insert_dates.map(inserted_date => artists.data.filter(artist => artist.inserted_date == inserted_date).length)
+            const artists_name = [...new Set(artists.data.map(artist => artist.artist_name))]
             const ctx = document.getElementById('artists').getContext('2d');
-            console.log(data);
-            if(artists_chart!=null){
-                artists_chart.destroy();
-            }
-            artists_chart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: insert_dates,
-                    datasets: [{
-                        label: 'Artists based on tags',
-                        data: data,
-                         backgroundColor: [
-                             'rgba(255, 99, 132, 0.2)',
-                             'rgba(54, 162, 235, 0.2)',
-                             'rgba(255, 206, 86, 0.2)',
-                             'rgba(75, 192, 192, 0.2)',
-                             'rgba(153, 102, 255, 0.2)',
-                             'rgba(255, 159, 64, 0.2)'
-                         ],
-                         borderColor: [
-                             'rgba(255, 99, 132, 1)',
-                             'rgba(54, 162, 235, 1)',
-                             'rgba(255, 206, 86, 1)',
-                             'rgba(75, 192, 192, 1)',
-                             'rgba(153, 102, 255, 1)',
-                             'rgba(255, 159, 64, 1)'
-                         ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            },
-                            gridLines: {
-                               display: false
-                            }
-                        }],
-
-                        xAxes: [{
-                            gridLines: {
-                                display: false
-                            }
-                        }]
+            var artists_ranks = []
+            var ranks = []
+            for (var j=0; j<artists_name.length; j++)
+            {
+                artists_ranks = {}
+                var datasets = []
+                for (var i=0; i<artists.data.length; i++)
+                {
+                    if((artists.data[i]["artist_name"] == artists_name[j]))
+                    {
+                        datasets.push(artists.data[i]["artist_rank"])
                     }
                 }
-            });
+                var randomcolor = colors[j]
+                artists_ranks.label = artists_name[j]
+                artists_ranks.data = datasets
+                artists_ranks.fill = false
+                artists_ranks.lineTension = 0
+                artists_ranks.radius = 5
+                artists_ranks.backgroundColor = randomcolor
+                artists_ranks.borderColor = randomcolor
+                artists_ranks.steppedLine = true
+                //artists_ranks.borderWidth = "50px"
+                ranks.push(artists_ranks)
+            }
+            line_chart(artists_chart, ctx, insert_dates, ranks)
+        }
+    });
+}
+
+function line_chart(chart, ctx, data_labels, dataset){
+    if(chart!=null){
+        chart.destroy();
+    }
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data_labels,
+            datasets: dataset
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    },
+                    gridLines: {
+                       display: true
+                    }
+                }],
+
+                xAxes: [{
+                    gridLines: {
+                        display: true
+                    }
+                }]
+            },
+            legend:{
+                display:true,
+                position:"bottom"
+            }
         }
     });
 }
